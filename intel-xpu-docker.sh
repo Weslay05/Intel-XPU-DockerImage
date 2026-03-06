4a82e67a3c6e36476cefacd9db05f140125a165b
Weslay05 <weslaytchibindi3@gmail.com>
2026-03-06 21:18:41 +0100

    Update intel-xpu-docker.sh

--- intel-xpu-docker.sh
+++ intel-xpu-docker.sh
@@ -5,6 +5,17 @@ docker run -it \
   -v $(pwd):/workspace \
   -w /workspace
   arc-xpu-env
+  
+# For conda python
+docker run -it \
+	--device /dev/dri \
+	-e environmentvariable=variable
+	-v $(pwd):/workspace \
+	-w "/workspace" \
+	-p 8188:8188 \
+	arc-xpu-env \
+	conda run --no-capture-output -p conda \
+	python main.py --listen 0.0.0.0;
 
 # Optionally : --group-add $(getent group render | cut -d: -f3)
 
